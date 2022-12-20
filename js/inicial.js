f4916617a15794308eb396a0bcc8172abdb77d1a
GamerCleanVic <jonassd2000@gmail.com>
2022-12-20 18:48:33 -0500

    +CSS&&FormJS

---
+++ js/inicial.js
@@ -0,0 +1,8 @@
+document.write(`
+    <form method="post" class="form1" id="formID1">
+        <input type="number" class="caixa1" /><br />
+        <input type="submit" value="Confirmar" class="btn1" />
+    </form>
+`);
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "ESTE É O TÍTULO DO TEXTO";
